b21870540732e9eaf6bd244e640a5bd2716aa57d
Gregor Billing <suushiemaniac@gmail.com>
2020-01-29 16:08:17 +0100

    Add back fancy version of PuzzlePlugins

---
+++ scrambles/src/main/java/org/worldcubeassociation/tnoodle/scrambles/LazyPuzzleSupplier.java
@@ -0,0 +1,58 @@
+package org.worldcubeassociation.tnoodle.scrambles;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+class LazyPuzzleSupplier<T extends Puzzle> {
+    private T instance;
+
+    public Class<T> supplyingClass;
+    protected Object[] ctorArgs;
+
+    public LazyPuzzleSupplier(Class<T> supplyingClass, Object... ctorArgs) {
+        this.supplyingClass = supplyingClass;
+        this.ctorArgs = ctorArgs;
+    }
+
+    public T getInstance() {
+        if (this.instance == null) {
+            this.instance = this.provideInstance();
+        }
+
+        return this.instance;
+    }
+
+    private T provideInstance() {
+        try {
+            Class<?>[] classes = this.getCtorArgClasses();
+            Constructor<T> constructor = this.supplyingClass.getConstructor(classes);
+            return constructor.newInstance(this.ctorArgs);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private Class<?>[] getCtorArgClasses() {
+        Class<?>[] ctorArgTypes = new Class[this.ctorArgs.length];
+
+        for (int i = 0; i < ctorArgTypes.length; i++) {
+            Object arg = this.ctorArgs[i];
+
+            if (arg instanceof Integer) {
+                ctorArgTypes[i] = int.class;
+            } else {
+                ctorArgTypes[i] = this.ctorArgs[i].getClass();
+            }
+        }
+
+        return ctorArgTypes;
+    }
+}
