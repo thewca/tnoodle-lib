2ccf898ba893fc549915f0533af7938c94a3a3c4
Gregor Billing <suushiemaniac@gmail.com>
2020-02-11 22:24:56 +0100

    Redirect threephase output to log

--- threephase/build.gradle.kts
+++ threephase/build.gradle.kts
@@ -1,6 +1,8 @@
+import configurations.Languages.attachRemoteRepositories
 import configurations.Languages.configureJava
 import configurations.Publications.configureBintrayTarget
 import configurations.Publications.configureMavenPublication
+import dependencies.Libraries.LOGBACK_CLASSIC
 
 description = "A copy of Chen Shuang's 4x4 scrambler."
 
@@ -14,6 +16,9 @@ configureJava()
 configureMavenPublication("scrambler-threephase")
 configureBintrayTarget()
 
+attachRemoteRepositories()
+
 dependencies {
     implementation(project(":min2phase"))
+    implementation(LOGBACK_CLASSIC)
 }
--- threephase/src/main/java/cs/threephase/Center1.java
+++ threephase/src/main/java/cs/threephase/Center1.java
@@ -1,5 +1,8 @@
 package cs.threephase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static cs.threephase.Util.*;
 import static cs.threephase.Moves.*;
 import java.util.*;
@@ -15,11 +18,12 @@ import java.util.*;
 */
 
 final class Center1 {
-	
+    private static Logger logger = LoggerFactory.getLogger(Center1.class);
+
 	static int[][] ctsmv = new int[15582][36];
 	static int[] sym2raw = new int[15582];
 	static byte[] csprun = new byte[15582];
-	
+
 	static int[][] symmult = new int[48][48];
 	static int[][] symmove = new int[48][36];
 	static int[] syminv = new int[48];
@@ -48,7 +52,7 @@ final class Center1 {
 				sym2raw[count++] = i;
 			}
 		}
-		assert count == 15582;		
+		assert count == 15582;
 	}
 
 	static void createPrun() {
@@ -56,7 +60,7 @@ final class Center1 {
 		csprun[0] = 0;
 		int depth = 0;
 		int done = 1;
-		
+
 		while (done != 15582) {
 			boolean inv = depth > 4;
 			int select = inv ? -1 : depth;
@@ -86,7 +90,7 @@ final class Center1 {
 	}
 
 	static void createMoveTable() {
-		System.out.println("Create Phase1 Center Move Table...");
+		logger.info("Create Phase1 Center Move Table...");
 		Center1 c = new Center1();
 		Center1 d = new Center1();
 		for (int i=0; i<15582; i++) {
@@ -98,51 +102,51 @@ final class Center1 {
 			}
 		}
 	}
-	
+
 	byte[] ct = new byte[24];
-	
+
 	Center1() {
 		for (int i=0; i<8; i++) {
 			ct[i] = 1;
 		}
 		for (int i=8; i<24; i++) {
 			ct[i] = 0;
-		}		
+		}
 	}
-	
+
 	Center1(byte[] ct) {
 		for (int i=0; i<24; i++) {
 			this.ct[i] = ct[i];
 		}
 	}
-	
+
 	Center1(CenterCube c, int urf) {
 		for (int i=0; i<24; i++) {
 			this.ct[i] = (byte) ((c.ct[i]/2 == urf) ? 1 : 0);
 		}
 	}
-	
+
 	void move(int m) {
 		int key = m % 3;
 		m /= 3;
 		switch (m) {
 		case 0:	//U
-			swap(ct, 0, 1, 2, 3, key);			
+			swap(ct, 0, 1, 2, 3, key);
 			break;
 		case 1:	//R
-			swap(ct, 16, 17, 18, 19, key);			
+			swap(ct, 16, 17, 18, 19, key);
 			break;
 		case 2:	//F
-			swap(ct, 8, 9, 10, 11, key);			
+			swap(ct, 8, 9, 10, 11, key);
 			break;
 		case 3:	//D
-			swap(ct, 4, 5, 6, 7, key);			
+			swap(ct, 4, 5, 6, 7, key);
 			break;
 		case 4:	//L
-			swap(ct, 20, 21, 22, 23, key);			
+			swap(ct, 20, 21, 22, 23, key);
 			break;
 		case 5:	//B
-			swap(ct, 12, 13, 14, 15, key);			
+			swap(ct, 12, 13, 14, 15, key);
 			break;
 		case 6:	//u
 			swap(ct, 0, 1, 2, 3, key);
@@ -173,10 +177,10 @@ final class Center1 {
 			swap(ct, 12, 13, 14, 15, key);
 			swap(ct, 1, 20, 7, 18, key);
 			swap(ct, 0, 23, 6, 17, key);
-			break;		
+			break;
 		}
-	}		
-	
+	}
+
 	void set(int idx) {
 		int r = 8;
 		for (int i=23; i>=0; i--) {
@@ -187,8 +191,8 @@ final class Center1 {
 			}
 		}
 	}
-	
-	int get() {	
+
+	int get() {
 		int idx = 0;
 		int r = 8;
 		for (int i=23; i>=0; i--) {
@@ -219,14 +223,14 @@ final class Center1 {
 	static int raw2sym(int n) {
 		int m = Arrays.binarySearch(sym2raw, n);
 		return (m>=0 ? m : -1);
-	}	
+	}
 
 	void set(Center1 c) {
 		for (int i=0; i<24; i++) {
 			this.ct[i] = c.ct[i];
 		}
 	}
-	
+
 	void rot(int r) {
 		switch (r) {
 		case 0:
@@ -250,8 +254,8 @@ final class Center1 {
 			move(dx3);
 			move(fx1);
 			move(bx3);
-			break;			
-		}	
+			break;
+		}
 	}
 /*
 0	I
@@ -279,8 +283,8 @@ final class Center1 {
 38	yz2
 39	y'z2
  */
-	static String[] rot2str = {"", "y2", "x", "x y2", "x2", "z2", "x'", "x' y2", "", "", "", "", "", "", "", "", 
-		"y z", "y' z'", "y2 z", "z'", "y' z", "y z'", "z", "z y2", "", "", "", "", "", "", "", "", 
+	static String[] rot2str = {"", "y2", "x", "x y2", "x2", "z2", "x'", "x' y2", "", "", "", "", "", "", "", "",
+		"y z", "y' z'", "y2 z", "z'", "y' z", "y z'", "z", "z y2", "", "", "", "", "", "", "", "",
 		"y' x'", "y x", "y'", "y", "y' x", "y x'", "y z2", "y' z2",  "", "", "", "", "", "", "", ""};
 
 
@@ -292,7 +296,7 @@ final class Center1 {
 			if (j%16==15) rot(3);
 		}
 	}
-	
+
 	static int getSolvedSym(CenterCube cube) {
 		Center1 c = new Center1(cube.ct);
 		for (int j=0; j<48; j++) {
@@ -313,7 +317,7 @@ final class Center1 {
 		}
 		return -1;
 	}
-	
+
 	public int hashCode() {
 		throw new UnsupportedOperationException();
 	}
@@ -330,7 +334,7 @@ final class Center1 {
 		}
 		return false;
 	}
-	
+
 	static void initSym() {
 		Center1 c = new Center1();
 		for (byte i=0; i<24; i++) {
@@ -339,7 +343,7 @@ final class Center1 {
 		Center1 d = new Center1(c.ct);
 		Center1 e = new Center1(c.ct);
 		Center1 f = new Center1(c.ct);
-		
+
 		for (int i=0; i<48; i++) {
 			for (int j=0; j<48; j++) {
 				for (int k=0; k<48; k++) {
@@ -351,7 +355,7 @@ final class Center1 {
 					}
 					d.rot(0);
 					if (k%2==1) d.rot(1);
-					if (k%8==7) d.rot(2);				
+					if (k%8==7) d.rot(2);
 					if (k%16==15) d.rot(3);
 				}
 				c.rot(0);
@@ -364,7 +368,7 @@ final class Center1 {
 			if (i%8==7) c.rot(2);
 			if (i%16==15) c.rot(3);
 		}
-		
+
 		for (int i=0; i<48; i++) {
 			c.set(e);
 			c.rotate(syminv[i]);
@@ -391,5 +395,5 @@ final class Center1 {
 			if (i%8==7) c.rot(2);
 			if (i%16==15) c.rot(3);
 		}
-	}	
+	}
 }
