b19500ceb76da37ef5fa4f4efc69f06afe2ba5cb
Gregor Billing <suushiemaniac@gmail.com>
2020-03-30 11:44:42 +0200

    Bump release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 allprojects {
     group = "org.worldcubeassociation.tnoodle"
-    version = "0.17.0"
+    version = "0.18.0"
 }
 
 plugins {
     DEPENDENCY_VERSIONS
 }
-
-val releasePrefix = "TNoodle-WCA"
 
 tasks.create("generateDebugRelease") {
     dependsOn(":scrambles:shadowJar")
