755b91b0aab2a3e56a7cd910f1f04edd6a3a3c91
Gregor Billing <gbilling@worldcubeassociation.org>
2021-04-12 21:45:45 +0200

    Configure GPG signing plugin

--- svglite/build.gradle.kts
+++ svglite/build.gradle.kts
@@ -2,6 +2,7 @@ import configurations.Languages.attachRemoteRepositories
 import configurations.Languages.configureJava
 import configurations.Frameworks.configureCheckstyle
 import configurations.Publications.configureMavenPublication
+import configurations.Publications.configureSignatures
 
 description = "A dead simple svg generation library written in pure Java, with no dependencies. This code runs on both desktop Java, Android, and compiles to Javascript with GWT."
 
@@ -11,8 +12,10 @@ plugins {
     `java-library`
     checkstyle
     `maven-publish`
+    signing
 }
 
 configureJava()
 configureCheckstyle()
 configureMavenPublication("lib-svglite")
+configureSignatures(publishing)
