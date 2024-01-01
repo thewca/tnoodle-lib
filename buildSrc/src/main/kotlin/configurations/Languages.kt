0fc83ebf111996309a56f469f36a48a3292554f6
Gregor Billing <gregor.billing.dev@gmail.com>
2024-01-01 18:50:55 +0100

    Compile as Java 8 bytecode
    Because Oracle still releases JRE8 on Windows

--- buildSrc/src/main/kotlin/configurations/Languages.kt
+++ buildSrc/src/main/kotlin/configurations/Languages.kt
@@ -2,6 +2,7 @@ package configurations
 
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.JavaVersion
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.repositories
@@ -18,6 +19,9 @@ object Languages {
             toolchain {
                 languageVersion.set(JavaLanguageVersion.of(11))
             }
+
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
 
             withJavadocJar()
             withSourcesJar()
